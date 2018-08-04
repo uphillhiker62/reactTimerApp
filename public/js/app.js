3a4607fe620126bfb86a85c1c19d9794e748a7b5
Cody Gates <cody.gates.89@gmail.com>
2018-08-03 22:30:34 -0600

    changed a little css

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,15 @@ class TimersDashboard extends React.Component {
     state = {
         timers: [
             {
-                title: 'Practice squat',
-                project: 'Gym Chores',
+                title: 'Run 1 mile',
+                project: 'Exercise',
                 id: uuid.v4(),
                 elapsed: 5456099,
                 runningSince: Date.now(),
             },
             {
-                title: 'Bake squash',
-                project: 'Kitchen Chores',
+                title: 'Eat a 20 oz. steak',
+                project: 'Food challenge',
                 id: uuid.v4(),
                 elapsed: 1273998,
                 runningSince: null,
@@ -219,7 +219,7 @@ class Timer extends React.Component {
                         </span>
                     </div>
                 </div>
-                <div className='ui bottom attached blue basic button'>
+                <div className='ui bottom attached green basic button'>
                     Start
                 </div>
             </div>
